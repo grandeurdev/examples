ede09bc04d0fa5d261d3fde354a8342d3e69b619
moizhusnain <moizhusnain@hotmail.com>
2020-10-05 05:50:30 +0500

    Added energy monitoring example

---
+++ Energy Monitoring/Hardware/Hardware.ino
@@ -0,0 +1,178 @@
+/* Including the SDK and WiFi library */
+#include <Apollo.h>
+#include <ESP8266WiFi.h>
+
+/* For NTP */
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+/* Configurations */
+String deviceID = "YOUR-DEVICE-ID";
+String apiKey = "YOUR-APIKEY";
+String token = "YOUR-ACCESS-TOKEN";
+
+/* WiFi credentials */
+String ssid = "WIFI-SSID";
+String password = "WIFI-PASSWORD";
+
+/* Create variable to hold project and device */
+Project project;
+Device device;
+
+/* Start NTP */
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+
+/* Define the pins */
+#define led 2
+
+/* Variable to keep track of connection state and time */
+int connected = 0;
+long lastUpdate = 0;
+
+/* Function to check device's connection status */
+void onConnection(bool status) {
+  switch(status) {
+        case CONNECTED:
+          /* 
+            Device connected to the cloud 
+            so turn off the led 
+          */
+          digitalWrite(led, 1);
+
+          Serial.println("Device is connected to the cloud.");
+
+          /* Record connection state and time */
+          connected = 1;
+        
+          lastUpdate = millis();
+          return;
+
+        case DISCONNECTED:
+          /* 
+            Device disconnected from cloud  
+            so turn on the led 
+          */
+          digitalWrite(led, 0);
+
+          Serial.println("Device is disconnected from the cloud.");
+
+          connected = 0;
+          return;
+  }
+}
+
+/* Function to connect to WiFi */
+void connectWiFi() {
+    /* Set mode to station */
+    WiFi.mode(WIFI_STA);
+
+    /* Connect using the ssid and password */
+    WiFi.begin(ssid, password);
+
+    /* Block till the WiFi is connected */
+    while (WiFi.status() != WL_CONNECTED) {
+         delay(500);
+         Serial.print(".");
+
+         /* Toogle the LED to represent that device are trying to connect to wifi*/
+         digitalWrite(led, !digitalRead(led));
+    }
+    
+    /* Print message */
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    /* Turn on the LED to represent the device is connecting to server*/
+    digitalWrite(led, 0);
+
+    /* And IP address */
+    Serial.println(WiFi.localIP());
+}
+
+/* Function to confirm that data has been updated */
+void summaryCallback(JSONObject setResult) {
+  /* If the update was recorded successfully */
+  if(setResult["code"] == "DEVICE-SUMMARY-UPDATED") {
+    /* Get data */
+    double current = (double) setResult["update"]["current"];
+    double power = (double) setResult["update"]["power"];
+
+    /* Print */
+    Serial.printf("Current: %f Power: %f\n", current, power);
+    
+    return;
+  }
+  
+  /* If the summary could not be updated. */
+  Serial.println("Failed to Update Summary");
+  return;
+}
+
+/* Function to send updated current and power readings */
+void sendUpdate() {
+    /* Send a new update after every 2 seconds */
+    if (millis() - lastUpdate > 2000) {
+      /* Add code for current measurement here */
+      float current;
+      float power;
+      
+      /* Update time */
+      timeClient.update();
+      
+      /* Epoch Time */
+      unsigned long time = timeClient.getEpochTime();
+      
+      // Form packet
+      JSONObject summary;
+
+      summary["current"] = current;
+      summary["power"] = power;
+      summary["time"] = time;
+
+      /* Record update */
+      device.setSummary(summary, summaryCallback);
+      
+      /* Record last record send time */
+      lastUpdate = millis();
+    }
+}
+
+void setup() {
+    /* Begin the serial */
+    Serial.begin(9600);
+
+    /* Connect to WiFi */
+    connectWiFi();
+    
+    /* Initializes the global object "apollo" with your configurations. */
+    project = apollo.init(apiKey, token);
+    
+    /* Get reference to device */
+    device = project.device(deviceID);
+    
+    /* Sets connection state update handler */
+    project.onConnection(onConnection);
+    
+    /* Set mode of LED pin to output */
+    pinMode(led, OUTPUT);
+
+    /* Setup pin state */    
+    digitalWrite(led, 1);
+
+    /* Get time */
+    timeClient.begin();
+
+    randomSeed(analogRead(0));
+}
+
+void loop() {
+    /* 
+        Synchronizes the SDK with the cloud
+        SDK will loop till we are connected to WiFi    
+    */
+    project.loop(WiFi.status() == WL_CONNECTED);
+
+    /* Send update to server */
+    if (connected) sendUpdate();
+}
