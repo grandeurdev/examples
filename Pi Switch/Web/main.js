57e9f05d95cc8f4da42a2d3a1801d7c2a5fe3a77
moizhusnain <moizhusnain@hotmail.com>
2020-11-04 04:37:15 +0500

    Added pi switch web app

---
+++ Pi Switch/Web/main.js
@@ -0,0 +1,144 @@
+/**
+ * @file: main.js
+ * Initialize the SDK and get 
+ * a reference to the project
+*/
+
+var project = apollo.init("YOUR-API-KEY", "YOUR-ACCESS-KEY", "YOUR-ACCESS-TOKEN");
+
+/** 
+ * This function uses the sdk to validate
+ * that if the user is authenticated or not
+*/
+async function start() {
+	/** Use sdk auth class to check auth status */
+	var res = await project.auth().isAuthenticated();
+
+	/** Then if the user isn't authorized then show the login screen */
+	if (res.code === "AUTH-UNAUTHORIZED") {
+		return displayLogin();
+	}
+
+	/** Display devices screen */
+	displayDevices();
+}
+
+/** Listener on login form button to authenticate a user */
+document.getElementById("submitLogin").addEventListener("click", async () => {
+	/** Get email and password from inputs */
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
+	/** Display laoder */
+	displayLoader();
+
+	/** Use the sdk auth class to login the user */
+	var res = await project.auth().login(email, password);
+
+	/** If the operation was successful */
+	if (res.code === "AUTH-ACCOUNT-LOGGEDIN") {
+		/** Reset the login page */
+		document.getElementById("email").value = "";
+		document.getElementById("password").value = "";
+		
+		/** Display devices screen */
+		return displayDevices();
+	}
+
+	/** otherwise display the login screen again */
+	displayLogin();
+});
+
+/** Function to get devices list from server and populate the ui*/
+async function getDevicesList() {
+	/** Use sdk devices class */
+	var devices = await project.devices();
+	var res = await devices.list();
+
+	/** Variable to hold the ui */
+	var content = "";
+
+	/** Then loop over the devices list returned in response and populate the ui */
+	res.devices.forEach(device => {
+		/** Add tile to the ui */
+		content += `
+			<div class="tile" onclick="updateState('${device.deviceID}')">
+				<div class="inner" id="${device.deviceID}" data-state="${device.parms.state}">
+					<div>${device.name}</div>
+					<img src="src/button-${device.parms.state? "on" : "off"}.svg" />
+				</div>
+			</div>
+		`
+
+		/** Then also subscribe to the state update event of the device */
+		devices.device(device.deviceID).onParms( parms => {
+			/** Update the tile color to represent that the device is on*/
+			document.getElementById(device.deviceID).getElementsByTagName("img")[0].src = `src/button-${parms.state? "on" : "off"}.svg`;
+
+			/** Update local attribute and store the latest state in it */
+			document.getElementById(device.deviceID).setAttribute("data-state", parms.state);
+		})
+	});
+
+	/** Assign content to ui */
+	document.getElementById("tiles").innerHTML = content;
+}
+
+/** Function to update the state of a device */
+async function updateState(deviceID) {
+	/** Create new state */
+	var newState = document.getElementById(deviceID).getAttribute("data-state") === "1" ? 0 : 1;
+
+	/** Use the devices class of sdk to report the upgrade */
+	await project.devices().device(deviceID).setParms({
+		state: newState
+	});
+}
+
+/** Add event handler on logout icon */
+document.getElementById("logout").addEventListener("click", async () => {
+	/** Show the loader */
+	displayLoader();
+
+	/** and use the auth class of sdk to logout the user */
+	await project.auth().logout();
+
+	/** Then call start again */
+	start();
+});
+
+/** Function to show laoder screen */
+function displayLoader() {
+	/** Display loader */
+	document.getElementById("loader").style.display = "flex";
+
+	/** Hide login screen */
+	document.getElementById("login").style.display = "none";
+
+	/** Hide devices screen */
+	document.getElementById("devices").style.display = "none";
+}
+
+/** Function to show login screen */
+function displayLogin() {
+	/** Hide loader */
+	document.getElementById("loader").style.display = "none";
+
+	/** Display login screen */
+	document.getElementById("login").style.display = "flex";
+}
+
+/** Function to show devices screen */
+function displayDevices() {
+	/** Hide loader */
+	document.getElementById("loader").style.display = "none";
+
+	/** Display devices screen */
+	document.getElementById("devices").style.display = "flex";
+
+	/** Get devices list */
+	getDevicesList();
+}
+
+/** Start the app */
+start();
